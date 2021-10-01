5521527d23e1fb067fa42007b86a0d1799ab57d2
Benjamin Nicholson <Vampiricaura66@gmail.com>
2021-10-01 23:27:25 +1000

    Finished file writer method, need to alter calculations before adding to hashmap

--- src/RainfallAnalyser.java
+++ src/RainfallAnalyser.java
@@ -1,62 +1,119 @@
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvException;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 public class RainfallAnalyser {
-    public static final int MONTH_INDEX = 3;
-    public static final int DAY_INDEX = 4;
-    public static final int RAIN_INDEX = 5;
+//    public static final int INDEX_YEAR = 2;
+    public static final int INDEX_MONTH = 3;
+    public static final int INDEX_DAY = 4;
+    public static final int INDEX_RAINFALL = 5;
 
     public static void main(String[] args) {
 
-        List<String[]> fileValues = new ArrayList<>();
-        HashMap<String, Double> monthMap = new HashMap<>();
-        HashMap<String, Double> dayMinMap = new HashMap<>();
-        HashMap<String, Double> dayMaxMap = new HashMap<>();
-
+        HashMap<String, Double> monthHashMap = new HashMap<>();
+        HashMap<String, Double> dayMinHasHMap = new HashMap<>();
+        HashMap<String, Double> dayMaxHashMap = new HashMap<>();
         try {
             FileReader fileReader = new FileReader(
-            "C:\\Users\\bensk\\IdeaProjects\\RainfallAnalyser\\src\\MountSheridanStationCNS.csv");
+                    "C:\\Users\\bensk\\IdeaProjects\\RainfallAnalyser\\src\\MountSheridanStationCNS.csv");
             CSVReader csvReader = new CSVReaderBuilder(fileReader).withSkipLines(1).build();
-            fileValues = csvReader.readAll();
+            List<String[]> fileContents = csvReader.readAll();
 
-
-            for (String[] row : fileValues) {
+            for (String[] row : fileContents) {
                 System.out.println(Arrays.toString(row));
-                collectMonths(monthMap, row);
-//                collectMinDays(dayMinMap, row);
-//                collectMaxDays(dayMaxMap, row);
+                insertMonths(monthHashMap, row);
+                insertDay(dayMinHasHMap, row, true);
+                insertDay(dayMaxHashMap, row, false);
             }
         } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
-        System.out.println();
+
+        System.out.println(monthHashMap);
+        System.out.println(dayMinHasHMap);
+        System.out.println(dayMaxHashMap);
+
+        String pathToFile = "C:\\Users\\bensk\\IdeaProjects\\RainfallAnalyser\\src\\MonthHashMap.csv";
+        String[] header = {"Month", "Total Rainfall"};
+        writeToCSV(monthHashMap, pathToFile, header);
+
+        pathToFile = "C:\\Users\\bensk\\IdeaProjects\\RainfallAnalyser\\src\\MinDayHashMap.csv";
+        header = new String[]{"Month-day", "Min Rainfall"};
+        writeToCSV(dayMinHasHMap, pathToFile, header);
+
+        pathToFile = "C:\\Users\\bensk\\IdeaProjects\\RainfallAnalyser\\src\\MaxDayHashMap.csv";
+        header = new String[]{"Month-day", "Max Rainfall"};
+        writeToCSV(dayMaxHashMap, pathToFile, header);
+    }
+
+    private static void writeToCSV(HashMap<String, Double> monthHashMap, String pathToFile, String[] header) {
+        try {
+            FileWriter writer = new FileWriter(pathToFile);
+            CSVWriter  csvWriter = new CSVWriter(writer);
+            csvWriter.writeNext(header);
+            for (Map.Entry<String, Double> entry : monthHashMap.entrySet()) {
+                String key = entry.getKey();
+                Double value = entry.getValue();
+                String[] row = {key, value.toString()};
+                csvWriter.writeNext(row);
+            }
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private static void collectMonths(HashMap<String, Double> monthMap, String[] row) {
-        String rainString = row[RAIN_INDEX];
-        String monthString = row[MONTH_INDEX];
+    private static void insertDay(HashMap<String, Double> monthMap, String[] row, boolean isMin) {
+        String monthString = row[INDEX_MONTH];
+        String rainfallString = row[INDEX_RAINFALL];
+        String dayString = row[INDEX_DAY];
+        String keyString = monthString + "-day-" + dayString;
         double rain = 0;
         try {
-            rain = Double.parseDouble(rainString);
+            rain = Double.parseDouble(rainfallString);
         } catch (Exception ignored) {}
 
-        if (monthMap.containsKey(monthString)) {
-            double oldRain = monthMap.get(monthString);
-            double newRain = oldRain + rain;
-            monthMap.put(monthString, newRain);
+        if (monthMap.containsKey(keyString)) {
+            double oldRainfall = monthMap.get(keyString);
+            double newRainfall;
+            if (isMin) {
+                newRainfall = Math.min(oldRainfall, rain);
+            } else {
+                newRainfall = Math.max(oldRainfall, rain);
+            }
+            monthMap.put(keyString, newRainfall);
+        } else {
+            monthMap.put(keyString, rain);
         }
-        else {
-            monthMap.put(monthString, rain);
+
+    }
+
+    private static void insertMonths(HashMap<String, Double> monthMap, String[] row) {
+        String monthString = row[INDEX_MONTH];
+        String rainfallString = row[INDEX_RAINFALL];
+        String keyString = "month- " + monthString + " rainfall " ;
+        double rain = 0;
+
+        try {
+            rain = Double.parseDouble(rainfallString);
+        } catch (Exception ignored) {
+        }
+
+        if (monthMap.containsKey(keyString)) {
+            double oldRain = monthMap.get(keyString);
+            double newRain = oldRain + rain;
+            monthMap.put(keyString, newRain);
+        } else {
+            monthMap.put(keyString, rain);
         }
     }
+
 }
